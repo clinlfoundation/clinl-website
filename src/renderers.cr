63bc6c29f8209b3f627cbeacb73215d8059ac16f
Ludovic 'Archivist' Lagouardette <archivist@172-0-0-153.lightspeed.brhmal.sbcglobal.net>
2019-03-31 14:18:50 +0200

    added 2 renderers

--- src/renderers.cr
+++ src/renderers.cr
@@ -1,26 +1,70 @@
+require "sqlite3"
+
 module Clinl::Website::Templates
+
+
+  class PageMeta
+    JSON.mapping(
+      name: String,
+      date: Date,
+      author: String
+    )
+  end
+
+  class PageContent
+    def initialize(@metadata : PageMeta, @content : String)
+    end
+
+    ECR.def_to_s "templates/page.ecr"
+  end
+
+  class Page
+    def initialize(@content : String)
+    end
+
+    ECR.def_to_s "templates/page_container.ecr"
+  end
+
   def page(name : String)
+    metadata = File.open("/data/pages/" + name + ".json") do |file|
+      PageMeta.from_json(file)
+    end
+	md_html = Markdown.parse(File.read("/data/pages/" + name + ".md")).to_html
 	
+	return Page.new(
+		PageContent.new(
+			metadata, md_html
+		).to_s
+	).to_s
   end
 
   def blogpost(name : String)
-
+    metadata = File.open("/data/blog/" + name + ".json") do |file|
+      PageMeta.from_json(file)
+    end
+    md_html = Markdown.parse(File.read("/data/blog/" + name + ".md")).to_html
+	
+	return Page.new(
+		PageContent.new(
+			metadata, md_html
+		).to_s
+	).to_s
   end
-  
+
   def summaries(skip : Int32)
+  
+  end
+
+  def links
 
   end
 
-  def links()
+  def events
 
   end
 
-  def events()
-
-  end
-
-  def votes()
-
+  def votes
+	
   end
 
   def finished_votes(skip : Int32)
@@ -31,4 +75,3 @@ module Clinl::Website::Templates
 
   end
 end
-  
